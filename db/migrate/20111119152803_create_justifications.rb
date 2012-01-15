fb55d278738d37f5ffae22035e2769ee1d815718
Nicolas Munoz <sauronnikko@gmail.com>
2012-01-15 16:06:38 -0430

    Fixing a migration

--- db/migrate/20111119152803_create_justifications.rb
+++ db/migrate/20111119152803_create_justifications.rb
@@ -2,7 +2,7 @@ class CreateJustifications < ActiveRecord::Migration
   def change
     create_table :justifications do |t|
       t.string :description, :null => false, :default => ""
-      t.boolean :is_justified, :null => false, :default => 0
+      t.boolean :is_justified, :null => false, :default => false
       t.integer :absence_id, :null => false, :default => 0
 
       t.timestamps
